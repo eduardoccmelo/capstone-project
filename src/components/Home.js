d90d4654a760f29c85790f72ee8c294168ba73d6
Eduardo Melo <eduardoccmelo@gmail.com>
2021-05-05 22:11:54 +0200

    remove home page header and add logo file

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="Home">
-      <div className="homePageHeader">Logo</div>
       <div className="homePageContent">
         <Link className="homeLink1" to="/myTrips">
           <div className="homePageLinksTitles">
