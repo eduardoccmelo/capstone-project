143811d743cac55b5cb8eeb816ea8b58208f7729
Eduardo Melo <eduardoccmelo@gmail.com>
2021-05-06 12:00:55 +0200

    create logo and place in the header

--- src/App.js
+++ src/App.js
@@ -7,31 +7,38 @@ import MyTrips from "./components/MyTrips";
 import NewTrip from "./components/NewTrip";
 import Trip from "./components/Trip";
 import WorldMap from "./components/WorldMap";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="header">LOGO</header>
+        <header className="header"></header>
         <main className="main">
           <Switch>
             <Route exact path="/">
               <Home className="firstPage" />
+              <Logo />
             </Route>
             <Route path="/myTrips/:id/edit">
               <EditTrip />
+              <Logo />
             </Route>
             <Route path="/myTrips/:id">
               <Trip />
+              <Logo />
             </Route>
             <Route path="/myTrips">
               <MyTrips />
+              <Logo />
             </Route>
             <Route path="/newTrip">
               <NewTrip />
+              <Logo />
             </Route>
             <Route path="/worldMap">
               <WorldMap />
+              <Logo />
             </Route>
             <Route path="*">
               <p>Page Not Found</p>
